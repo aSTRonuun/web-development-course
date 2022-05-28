3974a7ccbf84053f758138cf18c2e588ffb8bcf2
Vitor Alves <vitormanoel00710@gmail.com>
2022-05-28 16:49:48 -0300

    Added modal and logic js for the limited the tweet text

---
+++ 04-Bootstrap-Javascript/js/limiteMax.js
@@ -0,0 +1,28 @@
+function countCaracteres2() {
+
+    const countElement = document.querySelector('.count');
+    const textArea = document.getElementById('tweet-text');
+    const button = document.getElementById('button-send');
+
+    document.addEventListener('keyup', function() {
+        
+        const maxChar = 140;
+        const lenght = textArea.value.length;
+
+        if (lenght > 0 && lenght < 100) {
+            countElement.innerHTML = '<span>' + (maxChar - lenght)  + '</span>';
+            button.disabled = false;
+        }else if(lenght >= 100 && lenght <= 140) {
+            countElement.innerHTML = '<span class="alerrt">' + (maxChar - lenght)  + '</span>';
+            button.disabled = false;
+        } else if(lenght > 140) {
+            countElement.innerHTML = '<span class="maxAlert">' + (maxChar - lenght) + '</span>';
+            button.disabled = true;
+        }else {
+            countElement.innerHTML = '<span> </span>';
+        }
+    }) 
+    
+}
+
+countCaracteres2();
---
+++ 04-Bootstrap-Javascript/js/limiteMaxModal.js
@@ -0,0 +1,27 @@
+function countCaracteres1() {
+
+    const countElement = document.querySelector('.countM');
+    const textArea = document.getElementById('tweet-text-m');
+    const button = document.getElementById('button-send-modal');
+
+    document.addEventListener('keyup', function() {
+        
+        const maxChar = 140;
+        const lenght = textArea.value.length;
+        if (lenght > 0 && lenght < 100) {
+            countElement.innerHTML = '<span>' + (maxChar - lenght)  + '</span>';
+            button.disabled = false;
+        }else if(lenght >= 100 && lenght <= 140) {
+            countElement.innerHTML = '<span class="alerrt">' + (maxChar - lenght)  + '</span>';
+            button.disabled = false;
+        } else if(lenght > 140) {
+            countElement.innerHTML = '<span class="maxAlert">' + (maxChar - lenght) + '</span>';
+            button.disabled = true;
+        }else {
+            countElement.innerHTML = '<span> </span>';
+        }
+    }) 
+    
+}
+
+countCaracteres1();
